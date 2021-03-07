9fc816f37c940bc72df6e5fb1b1087f293fcfb54
Joshua Julius <joshjulius1@gmail.com>
2021-03-07 00:30:58 -0600

    add highlight on hover and label on click

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const inputCheck = document.querySelector('.input-check');
-const results = document.querySelector('.results');
+const airportName = document.querySelector('.airport-name');
+const results = document.querySelector('#results');
 // let decode = [];
 
-input.addEventListener('input', (e) => {
+
+/* -----------------
+FORM VALIDATION
+----------------- */
+
+input.addEventListener('input', () => {
     const query = input.value.toUpperCase();
+    input.value = query;
     inputCheck.style.display = 'inline';
-    if (input.value.length === 4) {
-        fetch(`https://api.aviationapi.com/v1/airports?apt=${query}`)
-            .then(checkStatus)    
-            .then(res => res.json())
-            .then(data => generateAirportName(data[query][0].facility_name))
-            .catch(generateErrorInput());
+    if (input.value.match(/[^A-Za-z]/)) {
+        inputCheck.innerHTML = `<span class="form-invalid">❌ Please enter alphabet characters only.</span>`;
     } else if (input.value.length === 0) {
         inputCheck.style.display = 'none';
+    } else if (!input.value.startsWith('K') && !input.value.startsWith('N') && !input.value.startsWith('P') && !input.value.startsWith('T')) {
+        inputCheck.innerHTML = `<span class="form-invalid">❌ US ICAO airport codes begin with K, N, P, or T.</span>`;
+    } else if (input.value.length === 4) {
+        fetch(`https://api.aviationapi.com/v1/airports?apt=${query}`)  
+            //SERVER ONLY RETURNS response: ok and status: 200
+            .then(res => res.json())
+            .then(checkStatusAirport)
+            .then(data => generateAirportName(data[query][0].facility_name, data[query][0].city))
+            .catch(err => generateErrorInput(err));
     } else {
-        inputCheck.innerHTML = `<span class="airport-name-invalid">❌ ${e.target.value} does not match any US airport ICAO codes.</span>`;
+        inputCheck.innerHTML = `<span class="form-typing">✏️ ${query}</span>`;
+    }
+
+    function checkStatusAirport(response) {
+        if (response[query] === []) {
+            return Promise.reject(new Error())
+        } else {
+            return Promise.resolve(response);
+        }
     }
 });
+
+
+
+/* -----------------
+FETCH METAR
+----------------- */
 
 form.addEventListener('submit', (e) => {
     const query = input.value.toUpperCase();
@@ -30,6 +56,36 @@ form.addEventListener('submit', (e) => {
         .catch(err => generateErrorSubmit(err));
 });
 
+
+
+/*------------------
+DISPLAY AIRPORT NAME AND CITY
+------------------*/
+form.addEventListener('submit', () => {
+    const query = input.value.toUpperCase();
+    inputCheck.style.display = 'inline';
+    fetch(`https://api.aviationapi.com/v1/airports?apt=${query}`)  
+            //SERVER ONLY RETURNS response: ok and status: 200
+            .then(res => res.json())
+            .then(checkStatusAirport)
+            .then(data => displayAirportName(data[query][0].facility_name, data[query][0].city))
+            .catch(err => generateErrorInput(err));
+
+    function checkStatusAirport(response) {
+        if (response[query] === []) {
+            return Promise.reject(new Error())
+        } else {
+            return Promise.resolve(response);
+        }
+    }
+});
+
+
+
+/* -----------------
+HELPER FUNCTIONS
+----------------- */
+
 function checkStatus(response) {
     if (response.ok) {
         return Promise.resolve(response);
@@ -38,8 +94,12 @@ function checkStatus(response) {
     }
 }
 
-function generateAirportName(airport) {
-    inputCheck.innerHTML = `<span class="airport-name-valid">✔️ ${airport}</span>`;
+function generateAirportName(airport, city) {
+    inputCheck.innerHTML = `<span class="form-valid">✔️ ${airport}, ${city}</span>`;
+}
+
+function displayAirportName(airport, city) {
+    airportName.innerHTML = `<p>${airport}, ${city}</p>`;
 }
 
 function generateHTML(data) {
@@ -55,13 +115,13 @@ function generateHTML(data) {
     } else if (data.raw.includes('00000KT')) {
         windDir = 'Not Applicable (Calm)'
     } else {
-        windDir = `${data.wind} degrees`;
+        windDir = `${data.wind} Degrees`;
     }
 
     if (gustExists) {
-        gust = `${gustExists[0][1]}${gustExists[0][2]} knots`;
+        gust = `Gusting ${gustExists[0][1]}${gustExists[0][2]}`;
     } else {
-        gust = 'None';
+        gust = '';
     }
 
     // check for rn sn br fg etc.
@@ -96,28 +156,57 @@ function generateHTML(data) {
     // checkMatch();
 
     results.innerHTML = `
-        <h4>METAR ${data.raw}</h4>
-        <p>Airport ID: ${data.station_id}</p>
-        <p>Time of Observation: ${time}</p>
-        <p>Wind Direction: ${windDir}</p>
-        <p>Wind Speed: ${data.wind_vel} knots</p>
-        <p>Wind Gust: ${gust}</p>
-        <p>Visibility: ${data.visibility} statute miles</p>
-        <p>Sky Condition: ${skyCondition.join(' / ').replace('FEW', ' few').replace('SCT', ' scattered').replace('BKN', ' broken').replace('OVC', ' overcast')}</p>
-        <p>Temperature: ${data.temp}°C</p>
-        <p>Dewpoint: ${data.dewpoint}°C</p>
-        <p>Altimeter Setting: ${data.alt_hg} inHg (${data.alt_mb} mb)</p>
+        <div id="raw-metar">
+            <h4>${addSpan()}</h4>
+        </div>
+        <div id="decoded-metar">
+            <p class="station-id">Airport ID: ${data.station_id}</p>
+            <p class="time">Time of Observation: ${time}</p>
+            <p class="wind">Wind Direction / Speed: ${windDir} / ${data.wind_vel} ${gust} Knots</p>
+            <p class="visibility">Visibility: ${data.visibility} Statute Miles</p>
+            <p class="sky-condition">Sky Condition: ${skyCondition.join(' - ').replace(/FEW/g, " Few").replace(/SCT/g, " Scattered").replace(/BKN/g, " Broken").replace(/OVC/g, " Overcast")}</p>
+            <p class="temp-dewpoint">Temperature / Dewpoint: ${data.temp}°C / ${data.dewpoint}°C</p>
+            <p class="altimeter-setting">Altimeter Setting: ${data.alt_hg} inHg (${data.alt_mb} mb)</p>
+        </div>
     `;
+
+    function addSpan() {
+        let items = data.raw.split(' ');
+        for (let i = 0; i < items.length ; i++) {
+            if (items[i] === data.station_id) {
+                items[i] = `<span class="station-id">${items[i]}</span>`;
+            } else if (items[i].match(/\d{6}Z/)) {
+                items[i] = `<span class="time">${items[i]}</span>`;
+            } else if (items[i].match(/\w*\d{2}KT/)) {
+                items[i] = `<span class="wind">${items[i]}</span>`;
+            } else if (items[i].match(/\d+SM/)) {
+                items[i] = `<span class="visibility">${items[i]}</span>`;
+            } else if (items[i].startsWith('CLR') || items[i].startsWith('FEW') || items[i].startsWith('SCT') || items[i].startsWith('BKN') || items[i].startsWith('OVC')) {
+                items[i] = `<span class="sky-condition">${items[i]}</span>`;
+            } else if (items[i].match(/^M?\d{2}\/M?\d{2}$/)) {
+                items[i] = `<span class="temp-dewpoint">${items[i]}</span>`;
+            } else if (items[i].match(/A\d{4}/)) {
+                items[i] = `<span class="altimeter-setting">${items[i]}</span>`;
+            } else {
+            items[i] = `<span>${items[i]}</span>`;
+            }
+        }
+        return items.join(' ');
+    }
 }
 
-function generateErrorInput() {
-    inputCheck.innerHTML = `<span class="airport-name-invalid">❌ ${input.value} does not match any US airport ICAO codes.</span>`;
+function generateErrorInput(err) {
+    if (err.toString().includes('facility_name')) {
+        inputCheck.innerHTML = `<span class="form-invalid">❌ ${input.value.toUpperCase()} does not match any US ICAO airport code.</span>`;
+    } else {
+        inputCheck.innerHTML = `<span class="form-invalid">❌ Unable to validate - ${err}</span>`;
+    }
 }
 
 function generateErrorSubmit(err) {
     if (input.value.length === 0) {
         inputCheck.style.display = 'inline';
-        inputCheck.innerHTML = `<span class="airport-name-invalid">☝️ Please enter an airport.</span>`;
+        inputCheck.innerHTML = `<span class="form-invalid">☝️ Please enter an airport.</span>`;
     }
 
     if (err.toString().includes(404)) {
@@ -127,14 +216,49 @@ function generateErrorSubmit(err) {
     }
 }
 
-// results.addEventListener('mouseover', (e) => {
-//     if (e.target.tagName == 'H4') {
-//         e.target.style.color = 'tomato';
-//     }
-// });
 
-// results.addEventListener('mouseout', (e) => {
-//     if (e.target.tagName == 'H4') {
-//         e.target.style.color = 'white';
-//     }
-// });
+
+/* -----------------
+HIGHLIGHT ON HOVER
+----------------- */
+
+results.addEventListener('mouseover', highlight);
+
+results.addEventListener('mouseout', removeHighlight);
+
+function highlight(e) {
+    let hoverTarget = document.getElementsByClassName(e.target.className);
+    for (let i = 0; i < hoverTarget.length; i++) {
+        hoverTarget[i].classList.add('active');
+    }
+}
+
+function removeHighlight() {
+        let hasActive = document.querySelectorAll('.active');
+        for (let i = 0; i < hasActive.length; i++) {
+            hasActive[i].classList.remove('active');
+        }
+}
+
+
+
+/*--------------------
+STICK LABEL
+-------------------*/
+
+results.addEventListener('click', addLabel);
+
+function addLabel(e) {
+    let labelTarget = document.getElementsByClassName(e.target.className.split(' ')[0]);
+    labelTarget[0].classList.add('label-sticker');
+    labelTarget[1].classList.add('label-bold');
+
+    let removeLabelFromSpan = document.querySelectorAll(`#raw-metar span:not(.${e.target.className.split(' ')[0]})`);
+    let removeLabelFromP = document.querySelectorAll(`#decoded-metar p:not(.${e.target.className.split(' ')[0]})`);
+    for (let i = 0; i < removeLabelFromSpan.length; i++) {
+        removeLabelFromSpan[i].classList.remove('label-sticker');
+    }
+    for (let i = 0; i < removeLabelFromP.length; i++) {
+        removeLabelFromP[i].classList.remove('label-bold');
+    }
+}
